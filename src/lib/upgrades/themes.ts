cc845836ce32a41cc863477fde62776585104d16
Roman Mendaliev <reacheight8@gmail.com>
2025-07-06 23:08:28 +0300

    3 new themes

--- src/lib/upgrades/themes.ts
+++ src/lib/upgrades/themes.ts
@@ -4,7 +4,7 @@ export const themeUpgrades: Upgrade[] = [
 	{
 		id: 'theme-dark',
 		name: 'Dark',
-		description: 'think twice',
+		description: 'I won\'t pay for your eyes treatment',
 		cost: 200,
 		type: 'theme',
 		value: 'dark',
@@ -17,6 +17,33 @@ export const themeUpgrades: Upgrade[] = [
 		cost: 250,
 		type: 'theme',
 		value: 'purple',
+		purchased: false
+	},
+	{
+		id: 'theme-ocean',
+		name: 'Ocean Breeze',
+		description: 'the only you can afford',
+		cost: 300,
+		type: 'theme',
+		value: 'ocean',
+		purchased: false
+	},
+	{
+		id: 'theme-forest',
+		name: 'Forest Zen',
+		description: 'embrace your inner tree hugger',
+		cost: 320,
+		type: 'theme',
+		value: 'forest',
+		purchased: false
+	},
+	{
+		id: 'theme-rose',
+		name: 'Rose Garden',
+		description: 'for real men 2',
+		cost: 280,
+		type: 'theme',
+		value: 'rose',
 		purchased: false
 	}
 ]; 
